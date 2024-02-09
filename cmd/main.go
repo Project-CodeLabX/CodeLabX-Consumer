2747983e60220dd7bbfa9fe0a91f5cf301a86f87
Abhi <abhishekvelekar79200@gmail.com>
2024-02-09 19:47:27 +0530

    file creation for code functionality added

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,16 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 var (
 	consumer *rmq.RmqConsumer
+	py       *os.File
+	java     *os.File
+	cpp      *os.File
 )
 
 func main() {
@@ -43,4 +47,39 @@ func main() {
 	}()
 
 	http.ListenAndServe(":9010", r)
+}
+
+func init() {
+	createFiles()
+	py.Close()
+	java.Close()
+	cpp.Close()
+	writeToFiles()
+}
+
+func createFiles() {
+	p, err := os.Create("res/codelabx.py")
+	if err != nil {
+		log.Println("error in py file creation: ", err)
+	}
+	j, err := os.Create("res/codelabx.java")
+	if err != nil {
+		log.Println("error in java file creation: ", err)
+	}
+	c, err := os.Create("res/codelabx.cpp")
+	if err != nil {
+		log.Println("error in cpp file creation: ", err)
+	}
+
+	py = p
+	java = j
+	cpp = c
+}
+
+func writeToFiles() {
+	py, _ := os.OpenFile("res/codelabx.py", os.O_WRONLY, 0666)
+	ans, err := py.WriteString("print(\"Hello from CodeLab\")")
+
+	log.Println("ans: ", ans)
+	log.Println("err: ", err)
 }
